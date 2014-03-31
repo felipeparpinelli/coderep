2216ff7df9aa210c8acef409b8959cac9f2c1be6
Felipe Parpinelli <felipeparpinelli@gmail.com>
2014-03-31 11:31:03 -0300

    create method generateJsonComponents

--- coderep.py
+++ coderep.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from providers import githubResources
 from providers import stackoverflowResouces
+from database import coderepdb
 
 app = Flask(__name__)
 
@@ -25,6 +26,38 @@ def stackoverflow():
 @app.route('/savecomp')
 def saveComponent():
     return 'ok'
+
+
+@app.route('/json')
+def generateJsonComponents():
+    components = coderepdb.getAllComponents()
+    languages = coderepdb.getAllLanguages()
+
+    keyName = "name"
+    valueName = "components"
+
+    keyChildren = "children"
+    valueChildren = []
+
+    keySize = "size"
+
+    array = []
+
+    for language in languages:
+        for component in components:
+            if language.id == component.language_id:
+                compDict = {keyName: component.name, keySize: component.rep}
+                array.append(compDict)
+
+        mutableDict = {keyName: language.name, keyChildren: array}
+        valueChildren.append(mutableDict)
+        array = []
+
+    dict = {keyName: valueName, keyChildren: valueChildren}
+    print(dict)
+
+    return 'ok'
+
 
 if __name__ == '__main__':
     app.debug = True
