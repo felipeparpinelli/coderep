a6b6f8a226f3f0ef75b60eb43b1d4880a8ebac0c
Felipe Parpinelli <felipe.parpinelli@tvglobo.com.br>
2014-03-31 22:01:20 -0300

    Configure svg in div content-svg

--- static/js/components.js
+++ static/js/components.js
@@ -15,10 +15,32 @@ var bubble = d3.layout.pack()
     .size([diameter, diameter])
     .padding(1.5);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#content-svg").append("svg")
     .attr("width", diameter)
     .attr("height", diameter)
     .attr("class", "bubble");
+
+d3.json("components.json", function(error, root) {
+  var node = svg.selectAll(".node")
+      .data(bubble.nodes(classes(root))
+      .filter(function(d) { return !d.children; }))
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+  node.append("title")
+      .text(function(d) { return d.className + ": " + format(d.value); });
+
+  node.append("circle")
+      .attr("r", function(d) { return d.r; })
+      .style("fill", function(d) { return color(d.packageName); });
+
+  node.append("text")
+      .attr("dy", ".3em")
+      .style("text-anchor", "middle")
+      .text(function(d) { return d.className.substring(0, d.r / 3); });
+});
+
 
 // Returns a flattened hierarchy containing all leaf nodes under the root.
 function classes(root) {
