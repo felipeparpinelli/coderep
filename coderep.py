fc23dee518bd48dc25aea6067655fab6da15e6e2
Felipe Parpinelli <felipeparpinelli@gmail.com>
2014-07-13 00:56:36 -0300

    add routes and refactoring

--- coderep.py
+++ coderep.py
@@ -1,98 +1,23 @@
 from flask import Flask, render_template, jsonify
-from providers import githubResources
-from providers import stackoverflowResouces
+from providers import github_resources
+from providers import stackoverflow_resouces
 from database import coderepdb
-from settings import APP_STATIC
-from models import registrationForm
+from models import component
+from models import language
 import urllib
 import json
 from flask import request
-import os
+import history
+from models import pending_component
 
 app = Flask(__name__)
 
-@app.route('/')
+comp = None
+array_langs = None
+
+@app.route('/health_check')
 def health_check():
     return 'Health_Check!'
-
-
-@app.route('/github')
-def github():
-    mockUrl = 'https://api.github.com/repos/django/django'
-    check_url = githubResources.check_valid("https://github.com/mbostock/d3")
-    stars = githubResources.get_stars(mockUrl)
-    return str(stars)
-
-
-@app.route('/so')
-def stackoverflow():
-    so = stackoverflowResouces.get_tags('django')
-    test = stackoverflowResouces.check_valid("http://api.stackoverflow.com/1.1/tags?filter=abcsdferg")
-    return str(so)
-
-
-@app.route('/savecomp')
-def save_component():
-    return 'ok'
-
-
-@app.route('/json')
-def generate_json_components():
-    components = coderepdb.get_all_components()
-    languages = coderepdb.get_all_languages()
-
-    key_name = "name"
-    value_name = "components"
-
-    key_children = "children"
-    children = []
-
-    key_size = "size"
-
-    array = []
-
-    for language in languages:
-        for component in components:
-            if language.id == component.language_id:
-                comp_dict = {key_name: component.name, key_size: component.rep}
-                array.append(comp_dict)
-
-        mutable_dict = {key_name: language.name, key_children: array}
-        children.append(mutable_dict)
-        array = []
-
-    dict = {key_name: value_name, key_children: children}
-    dict = json.dumps(dict, ensure_ascii=False)
-
-    return dict
-
-
-def update_stars():
-    components = coderepdb.get_all_components()
-
-    for component in components:
-        url = component.github_url
-        if url is not None:
-            url_parse_api = url.replace("https://github.com/", "https://api.github.com/repos/")
-            stars = githubResources.get_stars(url_parse_api)
-            if str(component.stars) != stars:
-                component.stars = stars
-                coderepdb.update_values(component)
-    return 'ok'
-
-
-def update_tags():
-    components = coderepdb.get_all_components()
-
-    for component in components:
-        tags = component.tags
-        if tags is not None:
-            tag = stackoverflowResouces.get_tags(component.name)
-            if component.tags != tag:
-                if component.tags > 0 and tag != 0:
-                    component.tags = tag
-                    coderepdb.update_values(component)
-    return 'ok'
 
 
 def update_rep():
@@ -110,22 +35,12 @@ def update_rep():
     return 'ok'
 
 
-@app.route('/writefile')
-def write_json_file():
-    jsonDict = generate_json_components()
-
-    with open(os.path.join(APP_STATIC, 'components.json'), 'w') as f:
-        f.write(jsonDict)
-        f.close
-
-    return 'ok'
-
-
-@app.route('/filterbylang/<lang>')
+@app.route('/filterbylang/<lang>', methods=['GET'])
 def filter_by_lang(lang):
-    with open(os.path.join(APP_STATIC, 'components.json'), 'r') as f:
-        dict = json.load(f)
-        f.close
+    global comp
+    if comp is None:
+        comp = component.generate_json_components()
+    dict = json.loads(comp)
 
     children = []
     dict_lang = None
@@ -141,10 +56,12 @@ def filter_by_lang(lang):
         return "Error - Not found language with name: " + lang
 
     children.append(dict_lang)
-    json_lang = {"name": "components", "children": children}
-    json_lang = json.dumps(json_lang, ensure_ascii=False)
-
-    return json_lang
+    ret = {"name": "components", "children": children}
+    ret2 = json.loads(history.filter_by_lang(lang))
+    j = []
+    j.append(ret)
+    j.append(ret2)
+    return jsonify(result=j)
 
 
 @app.route('/register', methods=['GET'])
@@ -152,13 +69,53 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/python', methods=['GET'])
+def python():
+    ret = {"msg": "Select a language", "error": 1}
+    return jsonify(ret), 200
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/getcomps', methods=['GET'])
+def get_comps():
+    global comp
+    comp = component.generate_json_components()
+    return comp
+
+
+@app.route('/gethistory', methods=['GET'])
+def get_history():
+    _history = history.create_json_history()
+    return _history
+
+
+@app.route('/getlanghistory', methods=['GET'])
+def get_lang_history():
+    _history = history.create_json_lang_history()
+    return _history
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+
+@app.route('/languages', methods=['GET'])
+def lang():
+    return render_template('language.html')
+
+
 @app.route('/submit', methods=['POST'])
 def submit():
 
     data = request.data
     data = data.split('&')
-    component = data[0].split('=')
-    component = component[1]
+    component_name = data[0].split('=')
+    component_name = component_name[1]
     github_url = data[1].split('=')
     github_url = github_url[1]
     github_url = urllib.unquote(github_url).decode('utf8')
@@ -169,11 +126,11 @@ def submit():
     is_gh_valid = True
     is_so_valid = True
 
-    if coderepdb.exist_component(component, github_url):
+    if coderepdb.exist_component(component_name, github_url):
         ret = {"msg": "That component already exists", "error": 1}
         return jsonify(ret), 200
 
-    if component is '' and str(github_url) is '':
+    if component_name is '' and str(github_url) is '':
         ret = {"msg": "Fill in at least one field", "error": 1}
         return jsonify(ret), 200
 
@@ -181,30 +138,79 @@ def submit():
         ret = {"msg": "Select a language", "error": 1}
         return jsonify(ret), 200
 
-    if not githubResources.check_valid(github_url) and github_url != '':
+    if not github_resources.check_valid(github_url) and github_url != '':
         ret = {"msg": "Please, enter a valid github url", "error": 1}
         return jsonify(ret), 200
 
-    github_url = github_url.replace("https://github.com/", "https://api.github.com/repos/")
-    github_stars = githubResources.get_stars(github_url)
-    lang = githubResources.lang
+    git_repo_name = github_resources.get_repo_name(github_url)
+
+    github_url_api = github_url.replace("https://github.com/", "https://api.github.com/repos/")
+    github_stars = github_resources.get_stars(github_url_api)
     if github_stars is 'error':
         is_gh_valid = False
 
-    if stackoverflowResouces.get_tags(component) is 0:
+    if stackoverflow_resouces.get_tags(component_name) is 0:
         is_so_valid = False
 
     if is_so_valid is False and is_gh_valid is False:
-        ret = {"msg": "No information found for " + component + ' ' + github_url, "error": 1}
+        ret = {"msg": "No information found for " + component_name + ' ' + github_url, "error": 1}
         return jsonify(ret), 200
 
     if not coderepdb.get_language_id(lang) and lang_id == str(-1):
         ret = {"msg": "Unfortunately we didn't find the " + lang + " language in our database. A request to add it was sent.", "error": 1}
         return jsonify(ret), 200
 
-    ret = {"msg": "OK!", "error": 0}
+    if git_repo_name == component_name and lang == github_resources.lang:
+        _comp = create_component(component_name, github_stars, github_url, lang)
+        ret = {"msg": "OK! Registered successfully.", "error": 0}
+        history.set_new_comp(_comp, lang)
+        coderepdb.insert_component(_comp)
+    else:
+        pendingComp = create_pending_comp(component_name, github_stars, github_url, lang)
+        ret = {"msg": "OK! Pending approval.", "error": 0}
+        coderepdb.insert_component(pendingComp)
+
     return jsonify(ret), 200
 
+
+def create_component(component_name, github_stars, github_url, lang):
+    _comp = component.Component(str(component_name), int(github_stars), int(stackoverflow_resouces.get_tags(component_name)),
+                                        int(int(github_stars) + int(int(stackoverflow_resouces.get_tags(component_name)))),
+                                        str(github_url), int(coderepdb.get_language_id(lang)))
+    _comp.name = str(component_name)
+    _comp.github_url = str(github_url)
+    _comp.stars = int(github_stars)
+    _comp.tags = int(stackoverflow_resouces.get_tags(component_name))
+    _comp.rep = int(float(_comp.stars) + float(_comp.tags))
+    _comp.language_id = int(coderepdb.get_language_id(lang))
+    print coderepdb.get_language_id(lang)
+
+    return _comp
+
+def create_pending_comp(component_name, github_stars, github_url, lang):
+    _comp = pending_component.PendingComponent(str(component_name), int(github_stars), int(stackoverflow_resouces.get_tags(component_name)),
+                                        int(int(github_stars) + int(int(stackoverflow_resouces.get_tags(component_name)))),
+                                        str(github_url), int(coderepdb.get_language_id(lang)))
+    _comp.name = str(component_name)
+    _comp.github_url = str(github_url)
+    _comp.stars = int(github_stars)
+    _comp.tags = int(stackoverflow_resouces.get_tags(component_name))
+    _comp.rep = int(float(_comp.stars) + float(_comp.tags))
+    _comp.language_id = int(coderepdb.get_language_id(lang))
+
+    return _comp
+
+
+@app.route('/searchlangs', methods=['GET'])
+def search_langs():
+    global array_langs
+    key = '/lang/' + history.get_date_sys()
+    value = history.get_history(key)
+    if value is None or array_langs is None:
+        history.set_lang_history()
+        array_langs = language.generate_json_languages()
+
+    return array_langs
 
 
 if __name__ == '__main__':
